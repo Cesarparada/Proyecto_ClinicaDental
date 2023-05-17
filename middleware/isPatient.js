3e5372da43ee2b63c5ae8f27d80123cdb529c7b4
César David Parada Castillo <cesard.0925@gmail.com>
2023-05-17 19:04:15 +0200

    modificacion en la comprobacion de rol

--- middleware/isPatient.js
+++ middleware/isPatient.js
@@ -1,7 +1,7 @@
 const { sendErrorResponse } = require("../_util/sendResponse");
 const isPatient = async (req, res, next) => {
   const { usuario_role } = req;
-  if (usuario_role != 1) {
+  if (usuario_role != "user") {
     return sendErrorResponse(res, 403, "No tiene los permisos necesarios");
   } else next();
 };
